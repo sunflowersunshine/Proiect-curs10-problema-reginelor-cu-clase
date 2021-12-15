00fe89c6a1a547ac1d748e17603e322e576fd86a
Monica-Mariana Stoleru <monicastoleru7@gmail.com>
2021-12-15 10:40:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_curs10
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("State the value of N in this program!");
+            N = Convert.ToInt32(Console.ReadLine());
+            int[,] board = new int[N, N];
+            var boardSolver =  new TestBack().theBoardSolver(board, 0, N);
+            if (!boardSolver)
+            {
+                Console.WriteLine("Solution not found.");
+            }
+            TestBack.printBoard(board, N);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestBack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_curs10
+{
+    public  class TestBack
+    {
+
+        public static void printBoard(int[,] board, int N)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+        public Boolean toPlaceOrNotToPlace(int[,] board, int row, int col, int N)
+        {
+            int i, j;
+            for (i = 0; i < col; i++)
+            {
+                if (board[row, i] == 1) return false;
+            }
+            for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i, j] == 1) return false;
+            }
+            for (i = row, j = col; j >= 0 && i < N; i++, j--)
+            {
+                if (board[i, j] == 1) return false;
+            }
+            return true;
+        }
+
+
+        public Boolean theBoardSolver(int[,] board, int col, int N)
+        {
+            if (col >= N) return true;
+            for (int i = 0; i < N; i++)
+            {
+                if (toPlaceOrNotToPlace(board, i, col, N))
+                {
+                    board[i, col] = 1;
+                    if (theBoardSolver(board, col + 1, N)) return true;
+                    // Backtracking is hella important in this one.  
+                    board[i, col] = 0;
+                }
+            }
+            return false;
+        }
+    }
+}
